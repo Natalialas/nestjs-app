0baa8176b9b762610cc05e8ed74e07a10fd0a48c
Natalia Laszczkowska <natalia.laszczkowskax@gmail.com>
2024-04-26 13:31:25 +0200

    Add endpoints to orders

---
+++ src/orders/dtos/create-order.dto.ts
@@ -0,0 +1,18 @@
+import { Transform } from 'class-transformer';
+import { IsNotEmpty, IsString } from 'class-validator';
+import { Product } from 'src/db';
+
+export class CreateOrderDTO {
+  @IsNotEmpty()
+  productId: Product['id'];
+
+  @IsNotEmpty()
+  @IsString()
+  @Transform(({ value }) => (Array.isArray(value) ? value.join(', ') : ''))
+  client: string;
+
+  @IsNotEmpty()
+  @IsString()
+  @Transform(({ value }) => (Array.isArray(value) ? value.join(', ') : ''))
+  address: string;
+}
--- src/orders/orders.service.ts
+++ src/orders/orders.service.ts
@@ -1,13 +1,28 @@
 import { Injectable } from '@nestjs/common';
 import { db, Order } from './../db';
+import { v4 as uuidv4 } from 'uuid';
 
 @Injectable()
 export class OrdersService {
   public getAll(): Order[] {
     return db.orders;
   }
-
   public getById(id: Order['id']): Order | null {
     return db.orders.find((p) => p.id === id);
+  }
+  public deleteById(id: Order['id']): void {
+    db.orders.filter((order) => order.id !== id);
+  }
+  public create(orderData: Omit<Order, 'id'>): Order {
+    const newOrder = { ...orderData, id: uuidv4() };
+    db.orders.push(newOrder);
+    return newOrder;
+  }
+  public updateById(id: Order['id'], orderData: Omit<Order, 'id'>): void {
+    db.orders.map((order) => {
+      if (order.id === id) {
+        return { ...order, ...orderData };
+      } else return order;
+    });
   }
 }
