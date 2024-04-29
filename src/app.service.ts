c10c096e2f65c16c654c6244440fc150141126a3
Natalia Laszczkowska <natalia.laszczkowskax@gmail.com>
2024-04-29 11:18:49 +0200

    x

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,8 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    const name = 'John';
-    const text = `Hello ${name}`;
-    return text;
+    return 'Hello World!';
   }
 }
